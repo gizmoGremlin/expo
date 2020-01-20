2c6f188fb2070bc74203aa8f4f06f1c14ea07fa7
Stanisław Chmiela <sjchmiela@gmail.com>
2020-01-20 16:33:20 +0100

    [expo-notifications] Implement permissions requesting methods

--- packages/expo-notifications/build/index.js
+++ packages/expo-notifications/build/index.js
@@ -2,4 +2,5 @@ export { default as getDevicePushTokenAsync } from './getDevicePushTokenAsync';
 export { default as getExpoPushTokenAsync } from './getExpoPushTokenAsync';
 export * from './TokenEmitter';
 export * from './NotificationsEmitter';
+export * from './NotificationPermissions';
 //# sourceMappingURL=index.js.map
