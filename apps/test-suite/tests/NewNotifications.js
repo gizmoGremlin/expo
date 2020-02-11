5263e4cb84f7bc7f79828fff43e7ac2f40b448e3
Stanisław Chmiela <sjchmiela@gmail.com>
2020-02-11 11:41:04 +0100

    [test-suite] Add notification received listener test

--- apps/test-suite/tests/NewNotifications.js
+++ apps/test-suite/tests/NewNotifications.js
@@ -71,6 +71,98 @@ export async function test(t) {
         t.expect(expoPushToken.type).toBe('expo');
         t.expect(typeof expoPushToken.data).toBe('string');
       });
+
+      // Not running those tests on web since Expo push notification doesn't yet support web.
+      const describeWithExpoPushToken = ['ios', 'android'].includes(Platform.OS)
+        ? t.describe
+        : t.xdescribe;
+
+      describeWithExpoPushToken('when a push notification is sent', () => {
+        let receivedEvent = null;
+        let subscription = null;
+
+        t.beforeAll(async () => {
+          let experienceId = undefined;
+          if (!Constants.manifest) {
+            // Absence of manifest means we're running out of managed workflow
+            // in bare-expo. @exponent/bare-expo "experience" has been configured
+            // to use Apple Push Notification key that will work in bare-expo.
+            experienceId = '@exponent/bare-expo';
+          }
+          const pushToken = await Notifications.getExpoPushTokenAsync({
+            experienceId,
+          });
+
+          subscription = Notifications.addNotificationReceivedListener(event => {
+            receivedEvent = event;
+          });
+
+          await sendTestPushNotification(pushToken.data);
+        });
+
+        t.afterAll(() => {
+          subscription.remove();
+        });
+
+        t.it('emits a “notification received” event', async () => {
+          let iterations = 0;
+          while (iterations < 5) {
+            iterations += 1;
+            if (receivedEvent) {
+              break;
+            }
+            await waitFor(1000);
+          }
+          t.expect(receivedEvent).not.toBeNull();
+        });
+      });
     });
   });
+}
+
+// In this test app we contact the Expo push service directly. You *never*
+// should do this in a real app. You should always store the push tokens on your
+// own server or use the local notification API if you want to notify this user.
+const PUSH_ENDPOINT = 'https://expo.io/--/api/v2/push/send';
+
+async function sendTestPushNotification(expoPushToken, notificationOverrides) {
+  // POST the token to the Expo push server
+  const response = await fetch(PUSH_ENDPOINT, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify([
+      {
+        to: expoPushToken,
+        title: 'Hello from Expo server!',
+        ...notificationOverrides,
+      },
+    ]),
+  });
+
+  const result = await response.json();
+  if (result.errors) {
+    for (const error of result.errors) {
+      console.warn(`API error sending push notification:`, error);
+    }
+    throw new Error('API error has occurred.');
+  }
+
+  const receipts = result.data;
+  if (receipts) {
+    const receipt = receipts[0];
+    if (receipt.status === 'error') {
+      if (receipt.details) {
+        console.warn(
+          `Expo push service reported an error sending a notification: ${receipt.details.error}`
+        );
+      }
+      if (receipt.__debug) {
+        console.warn(receipt.__debug);
+      }
+      throw new Error(`API error has occurred: ${receipt.details.error}`);
+    }
+  }
 }
